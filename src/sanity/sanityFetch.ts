da76792a007a931c8061f3921c3dbe8d51e95272
Krihages <kristian.hageseter+github@gmail.com>
2024-12-15 20:02:49 +0100

    fix for build issue

--- src/sanity/sanityFetch.ts
+++ src/sanity/sanityFetch.ts
@@ -1,18 +1,18 @@
 import { defineQuery } from "next-sanity";
 import { client } from "./client";
 
-export default async function sanityFetch(language: string) {
+export default async function sanityFetch(language: string, slug?: string) {
   const query = defineQuery(
-    `*[_type == "page" && language == "${language}" ][0]{
-  content,  
-  language,
-  navigation,
-  
-  "_translations": *[_type == "translation.metadata" && references(^._id)].translations[].value->{
-    slug,
-    language, 
-  }
-}`
+    `*[_type == "page" && language == "${language}"${slug ? ` && slug.current == "${slug}"` : ""}][0]{
+      content,  
+      language,
+      navigation,
+      
+      "_translations": *[_type == "translation.metadata" && references(^._id)].translations[].value->{
+        slug,
+        language, 
+      }
+    }`
   );
 
   const data = await client.fetch(query);
